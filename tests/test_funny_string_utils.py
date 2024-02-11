9c1a49a28a3aa5428ed2494d8344bd12de490c51
Jedex2 <Apirug.pr@gmail.com>
2024-02-11 22:28:13 +0700

    Update test_funny_string_utils.py

--- tests/test_funny_string_utils.py
+++ tests/test_funny_string_utils.py
@@ -2,14 +2,13 @@ from funny_string.funny_string_utils import funnyString
 import unittest
 
 class Funny_String(unittest.TestCase):
-    def test_give_acxz_should_Funny(self):
-        s = "acxz"
+    def test_give_uvzxrumuztyqyvpnji_should_Funny(self):
+        s = "uvzxrumuztyqyvpnji"
         expected_output = "Funny"
         
         result = funnyString(s)
         
         self.assertEqual(result,expected_output, f'Should be {expected_output}')
-
     def test_give_bcxz_should_Not_Funny(self):
         s = "bcxz"
         expected_output = "Not Funny"
@@ -42,8 +41,8 @@ class Funny_String(unittest.TestCase):
         
         self.assertEqual(result,expected_output, f'Should be {expected_output}')
     
-    def test_give_uvzxrumuztyqyvpnji_should_Funny(self):
-        s = "uvzxrumuztyqyvpnji"
+    def test_give_acxz_should_Funny(self):
+        s = "acxz"
         expected_output = "Funny"
         
         result = funnyString(s)
